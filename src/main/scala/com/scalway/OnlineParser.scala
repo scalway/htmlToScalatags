8d9d2106def912c3f6d7cd1db3291197c769f40d
Krzysztof Pecyna <krzysztof@qualityc.co>
2016-01-27 15:40:55 +0100

    change views

--- src/main/scala/com/scalway/OnlineParser.scala
+++ src/main/scala/com/scalway/OnlineParser.scala
@@ -4,7 +4,7 @@ import org.scalajs.dom
 import org.scalajs.dom.raw.MouseEvent
 import scalatags.JsDom.tags2.nav
 import scalatags.JsDom.all._
-
+import scalatags.JsDom.{attrs => ^, tags2 => <, TypedTag}
 import scala.scalajs.js.JSApp
 import scala.scalajs.js.annotation.JSExport
 
@@ -23,12 +23,17 @@ class OnlineParser extends JSApp {
     dom.document.body.appendChild(mainPageView.render)
   }
 
-  val htmlTextArea = textarea(rows:="4", cols:="50").render
-  val submit       = button("translate").render
-  val result       = textarea(rows:="4", cols:="50").render
+  val htmlTextArea = textarea(cls:="col-md-6", style:="min-height:300px").render
+  val result       = textarea(cls:="col-md-6", style:="min-height:300px").render
+
+  val submit       = button(
+      cls:="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" ,
+      "translate"
+    ).render
 
 
-  def mainPageView() = div(
+
+  def b3HeaderMenu(header:Tag, menuItems:TypedTag[dom.html.LI]*) = {
     nav( cls:="navbar navbar-inverse navbar-fixed-top",
       div( cls:="container",
         div( cls:="navbar-header",
@@ -38,25 +43,36 @@ class OnlineParser extends JSApp {
             span(cls:="icon-bar"),
             span(cls:="icon-bar")
           ),
-          a(cls:="navbar-brand", href:="#", "Project name")
+          header(cls:="navbar-brand")
         ),
         div( id:="navbar", cls:="collapse navbar-collapse",
           ul( cls:="nav navbar-nav",
-            li(cls:="active", a(href:="#","Home")),
-            li( a(href:="#about", "About")),
-            li( a(href:="#contact", "Contact"))
+            menuItems.toSeq
           )
         )
       )
+    )
+  }
+
+  def mainPageView() = div(
+    b3HeaderMenu(
+      a(href:="#", "HTML => SCALATAGS")
+//      ,li(cls:="active", a(href:="#","Home")),
+//      ,li(a(href:="#about", "About")),
+//      ,li(a(href:="#contact", "Contact"))
     ),
-    div( cls:="container",
-      div( cls:="starter-template"
-        ,h1("Bootstrap starter template")
-        ,p( cls:="lead", "Use this document as a way to quickly start any new project. All you get is this text and a mostly barebones HTML document.")
-        ,htmlTextArea
-        ,submit
-        ,result
+    div( cls:="container"
+      ,style:="padding-top:60px;"
+      ,div( cls:="row"
+        ,h1("HTML to SCALATAGS ONLINE CONVERTER", br(), small("Everyone sometimes want's to copy/paste something :)"))
+        ,p( cls:="lead", "Use this console to create scalatags code just from html tags. It isn't perfect tool but it works pretty well. " +
+          "You'll be probably obligated to make some additionals changes after convertion.")
       )
+      ,div(cls:="row"
+         ,htmlTextArea
+         ,result
+      )
+      ,div(cls:="row",(div(cls:="text-center",submit)))
     )
   )
 }
